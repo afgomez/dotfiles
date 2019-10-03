165c3dc00a59ec9a1cce65d72bf5dcd4d3fa16f3
Alejandro Fernández Gómez <antarticonorte@gmail.com>
2019-10-03 23:07:08 +0200

    Set CLICOLOR only in mac

--- .zshrc.d/00.env.zsh
+++ .zshrc.d/00.env.zsh
@@ -1,8 +1,12 @@
+system=`uname -s`
+
 # Always use English
 export LANG=en_US.UTF-8
 
-# Use colors in LS
-export CLICOLOR=1
-
 # Use the one and only terminal editor
 export EDITOR=vim
+
+if [ $system=="Darwin" ]; then
+  # Use colors in LS
+  export CLICOLOR=1
+fi
