1f59abdef15ed86705045125c1d5e334368522bc
Alejandro Fernández Gómez <antarticonorte@gmail.com>
2019-10-03 23:31:29 +0200

    Add XDG paths to use them in scripts

--- .zshrc.d/00.env.zsh
+++ .zshrc.d/00.env.zsh
@@ -6,6 +6,10 @@ export LANG=en_US.UTF-8
 # Use the one and only terminal editor
 export EDITOR=vim
 
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_DATA_HOME=$HOME/.local/share
+export XDG_CACHE_HOME=$HOME/.cache
+
 if [ $system=="Darwin" ]; then
   # Use colors in LS
   export CLICOLOR=1
