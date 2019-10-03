3c6f163049cf45f9d6dddd81107bf089a25de36e
Alejandro Fernández Gómez <antarticonorte@gmail.com>
2019-10-03 23:08:23 +0200

    Tweak behaviour of `cd` command

---
+++ .zshrc.d/01.cd.zsh
@@ -0,0 +1,12 @@
+# Autocomplete code projects
+cdpath=($HOME/code)
+
+# Treat directories as commands
+setopt autocd
+
+# Load `z` from homebrew if available
+if [[ -e "/usr/local/etc/profile.d/z.sh" ]]; then
+  . /usr/local/etc/profile.d/z.sh
+  [[ ! -e "$HOME/.z" ]] && touch $HOME/.z
+fi
+
