26f12432852a336ba08d59e69d346f7342f6ab8c
Alejandro Fernández Gómez <alejandro.fernandez@elastic.co>
2020-02-17 16:13:30 +0100

    Add shortcut to move to next screen

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -40,3 +40,23 @@ end)
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "M", function()
   hs.window.focusedWindow():move({ x = 0.00, y = 0.00, w = 1.00, h = 1.00 }, nil, true)
 end)
+
+
+-- Move to next screen
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "N", function()
+  -- Get the focused window, its window frame dimensions, its screen frame dimensions,
+  -- and the next screen's frame dimensions.
+  local focusedWindow = hs.window.focusedWindow()
+  local focusedScreenFrame = focusedWindow:screen():frame()
+  local nextScreenFrame = focusedWindow:screen():next():frame()
+  local windowFrame = focusedWindow:frame()
+
+  -- Calculate the coordinates of the window frame in the next screen and retain aspect ratio
+  windowFrame.x = ((((windowFrame.x - focusedScreenFrame.x) / focusedScreenFrame.w) * nextScreenFrame.w) + nextScreenFrame.x)
+  windowFrame.y = ((((windowFrame.y - focusedScreenFrame.y) / focusedScreenFrame.h) * nextScreenFrame.h) + nextScreenFrame.y)
+  windowFrame.h = ((windowFrame.h / focusedScreenFrame.h) * nextScreenFrame.h)
+  windowFrame.w = ((windowFrame.w / focusedScreenFrame.w) * nextScreenFrame.w)
+
+  -- Set the focused window's new frame dimensions
+  focusedWindow:setFrame(windowFrame)
+end)
