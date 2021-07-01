a6a28993ec1e5f68ee97ba2438a7d663276af3ac
Alejandro Fernandez Gomez <antarticonorte@gmail.com>
2021-07-01 18:21:40 +0200

    Setup the correct zsh on Apple silicon machines

--- .yadm/bootstrap.d/99.shell.zsh
+++ .yadm/bootstrap.d/99.shell.zsh
@@ -1,10 +1,19 @@
-if [[ $SHELL != "/usr/local/bin/zsh" ]]; then
-  echo '❯ Set `zsh` as your shell'
-
-  if [ ! `grep /usr/local/bin/zsh /etc/shells` ]; then
-    echo "/usr/local/bin/zsh" | sudo tee -a /etc/shells
-  fi
-
-  chsh -s /usr/local/bin/zsh
+if [ -f /opt/homebrew/bin/zsh ]; then
+  ZSH_BIN=/opt/homebrew/bin/zsh
 fi
 
+if [ -f /usr/local/bin/zsh ]; then
+  ZSH_BIN=/usr/local/bin/zsh
+fi
+
+
+
+if [[ $SHELL != $ZSH_BIN ]]; then
+  echo "❯ Set `zsh` as your shell ($ZSH_BIN)"
+
+  i [ ! `grep $ZSH_BIN /etc/shells` ]; then
+    echo $ZSH_BIN | sudo tee -a /etc/shells
+  fi
+
+  chsh -s $ZSH_BIN
+fi
