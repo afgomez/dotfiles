98dc8a10fd1c1fcd588ec2dae1ef9e904e632aec
Alejandro Fernandez Gomez <antarticonorte@gmail.com>
2021-07-07 19:02:47 +0200

    Move bootstrap.d directory

--- .config/yadm/bootstrap.d/03.iterm2.zsh
+++ .config/yadm/bootstrap.d/03.iterm2.zsh
@@ -2,4 +2,5 @@ system=`uname -s`
 if [ $system=Darwin ]; then
   echo "❯ Setting iTerm preference folder"
   defaults write com.googlecode.iterm2 PrefsCustomFolder "$HOME/.config/iterm"
+  defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 fi
