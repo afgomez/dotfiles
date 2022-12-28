64e7c7221d9721fee59a586fa0bfe55c2827d4fe
Alejandro Fernández Gómez <alejandro.fernandez@elastic.co>
2022-12-28 14:11:50 +0100

    Update asdf loader

--- .zshrc.d/10.extras.zsh
+++ .zshrc.d/10.extras.zsh
@@ -1,6 +1,6 @@
 function() {
   # ASDF version manager
-  local asdf_sh=$(brew --prefix asdf)/asdf.sh
+  local asdf_sh=$(brew --prefix asdf)/libexec/asdf.sh
 
   if [[ -e $asdf_sh ]]; then
     . $asdf_sh
