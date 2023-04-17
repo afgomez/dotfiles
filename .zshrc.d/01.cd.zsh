8051a8133c59766a5357a41ecc3b4f4bf725efc3
Alejandro Fernandez Gomez <alejandro@seenons.com>
2023-04-17 17:02:53 +0200

    Fix `z` command

--- .zshrc.d/01.cd.zsh
+++ .zshrc.d/01.cd.zsh
@@ -5,8 +5,7 @@ cdpath=(. $HOME/code)
 setopt autocd
 
 # Load `z` from homebrew if available
-if [[ -e "/usr/local/etc/profile.d/z.sh" ]]; then
-  . /usr/local/etc/profile.d/z.sh
+if [[ -e "/opt/homebrew/etc/profile.d/z.sh" ]]; then
+  . /opt/homebrew/etc/profile.d/z.sh
   [[ ! -e "$HOME/.z" ]] && touch $HOME/.z
 fi
-
