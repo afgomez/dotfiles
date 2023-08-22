dc417c509a04390bb851bfb0bc4f0d0acae29f1c
Alejandro Fernandez Gomez <antarticonorte@gmail.com>
2023-08-22 13:08:09 +0200

    Use better characters for the indent guides

--- .config/nvim/lua/plugins/indent_blankline.lua
+++ .config/nvim/lua/plugins/indent_blankline.lua
@@ -1,6 +1,8 @@
 return {
    "lukas-reineke/indent-blankline.nvim",
    opts = {
+      char = "▏",
+      context_char = "▏",
       show_current_context = true,
       show_trailing_blankline_indent = false,
    }
