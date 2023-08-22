d0d7f7b737b20c31e9201e74f038d05aa99cb327
Alejandro Fernandez Gomez <antarticonorte@gmail.com>
2023-08-22 21:49:43 +0200

    Close more aggresively the quick and location lists

--- .config/nvim/after/ftplugin/qf.lua
+++ .config/nvim/after/ftplugin/qf.lua
@@ -2,3 +2,6 @@ vim.opt_local.scrolloff = 0
 vim.opt_local.number = false
 vim.opt_local.cursorline = true
 vim.opt_local.textwidth = 0
+
+vim.keymap.set('n', '<Esc>', ':cclose<bar>lclose<CR>', { buffer = true, silent = true })
+vim.keymap.set('n', '<CR>', '<CR> :cclose<bar>lclose<CR>', { buffer = true, silent = true, noremap = true })
