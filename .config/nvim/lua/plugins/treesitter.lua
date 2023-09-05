ed8199915b838ad17651f98c69db4f1bb52e7416
Alejandro Fernandez Gomez <antarticonorte@gmail.com>
2023-09-05 08:45:39 +0200

    Disable TS `f` and `t` movements

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -88,18 +88,19 @@ return {
          },
       })
 
-      local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
-
-      -- Repeat movement with ; and ,
-      -- TODO explore https://github.com/ghostbuster91/nvim-next
-      -- ensure ; goes forward and , goes backward regardless of the last direction
-      vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
-      vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
-
-      -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
-      vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
-      vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
-      vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
-      vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
+      -- Disabled until https://github.com/nvim-treesitter/nvim-treesitter-textobjects/issues/488 gets resolved
+      --
+      -- -- local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+      -- -- Repeat movement with ; and ,
+      -- -- TODO explore https://github.com/ghostbuster91/nvim-next
+      -- -- ensure ; goes forward and , goes backward regardless of the last direction
+      -- -- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+      -- -- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
+      -- --
+      -- -- -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
+      -- -- vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
+      -- -- vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
+      -- -- vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
+      -- -- vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
    end,
 }
