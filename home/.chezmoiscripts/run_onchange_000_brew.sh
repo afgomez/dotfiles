2d507da97d828a689ed55bad549a6b7e177e7a59
Alejandro Fernández Gómez <antarticonorte@gmail.com>
2025-03-04 12:01:35 +0100

    Add homebrew

---
+++ home/.chezmoiscripts/run_onchange_000_brew.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+if ! command -v brew >/dev/null 2>&1; then
+  echo "❯ I could not find \`brew\` on the path."
+  echo "❯ Installing..."
+
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+echo "❯ Installing homebrew formulae..."
+brew bundle --file=- <<EOF
+
+# Commands
+brew "asdf"
+brew "bat"
+brew "difftastic"
+brew "fd"
+brew "git"
+brew "jj"
+brew "jq"
+brew "less"
+brew "neovim"
+brew "ripgrep"
+brew "tokei"
+brew "tree"
+brew "zoxide"
+brew "zsh-completions"
+
+# Dependencies
+brew "autoconf"
+brew "gmp"
+brew "libyaml"
+brew "readline"
+
+# Applications
+cask "appcleaner"
+cask "ghostty"
+cask "imageoptim"
+cask "maccy"
+cask "visual-studio-code"
+cask "zed"
+
+# Fonts
+cask "font-iosevka"
+cask "font-jetbrains-mono"
+
+
+EOF
