8a036b503dad39e8456ecae8f29488160fd4f1ea
Alejandro Fernández Gómez <antarticonorte@gmail.com>
2025-05-10 11:47:23 +0200

    No need to install latest runtimes, since we use homebrew node for the system

--- home/.chezmoiscripts/run_onchange_030_vfox.sh
+++ home/.chezmoiscripts/run_onchange_030_vfox.sh
@@ -7,9 +7,6 @@ for runtime in $RUNTIMES; do
   echo "❯ Installing vfox($runtime)..."
 
   vfox add "$runtime"
-
-  # Install the latest version of each runtime
-  vfox install "$runtime@latest"
 done
 
 # vfox exits with `1` if a runtime is already installed and stops chezmoi from continuing
