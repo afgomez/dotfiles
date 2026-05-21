609c5029624c7a070c21c99110de022d16807e44
Alejandro Fernández Gómez <antarticonorte@gmail.com>
2026-05-21 09:23:43 +0200

    Set up pnpm paths

--- home/dot_zshrc.d/00.env.zsh
+++ home/dot_zshrc.d/00.env.zsh
@@ -25,6 +25,9 @@ export ASDF_DATA_DIR="$XDG_DATA_HOME/asdf"
 export ASDF_CONFIG_FILE="$XDG_CONFIG_HOME/asdf/asdfrc"
 export ASDF_NODEJS_LEGACY_FILE_DYNAMIC_STRATEGY=latest_available
 
+# JS
+export PNPM_HOME="$XDG_DATA_HOME/pnpm"
+
 # Homebrew in Apple Silicon
 if [ -f /opt/homebrew/bin/brew ]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
--- home/dot_zshrc.d/01.path.zsh
+++ home/dot_zshrc.d/01.path.zsh
@@ -1,1 +1,1 @@
-path=($HOME/bin $ASDF_DATA_DIR/shims $(brew --prefix rustup)/bin $path)
+path=($HOME/bin $ASDF_DATA_DIR/shims $(brew --prefix rustup)/bin $PNPM_HOME/bin $path)
