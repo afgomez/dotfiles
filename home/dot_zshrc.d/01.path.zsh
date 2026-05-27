f973e404a6af9c6a9fc8ba7a48ac07b334e256da
Alejandro Fernández Gómez <antarticonorte@gmail.com>
2026-05-27 06:11:20 +0200

    Add `.cargo/bin` to path

--- home/dot_zshrc.d/01.path.zsh
+++ home/dot_zshrc.d/01.path.zsh
@@ -1,1 +1,8 @@
-path=($HOME/bin $ASDF_DATA_DIR/shims $(brew --prefix rustup)/bin $PNPM_HOME/bin $path)
+path=(
+  $HOME/bin
+  $ASDF_DATA_DIR/shims
+  $(brew --prefix rustup)/bin
+  $PNPM_HOME/bin
+  $HOME/.cargo/bin
+  $path
+)
